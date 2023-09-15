2fba36aee80082ac53dce6130daa91e6b5763ce6
tamnas <86242784+tamnas@users.noreply.github.com>
2023-09-15 16:53:40 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+  var skills = ['HTML', 'CSS', 'JS', 'React', 'Redux'];
+  console.log(skills);
+  console.log(skills.length);
+  console.log(skills[skills.length - 1]);
+  console.log(skills[skills.length - 2]);
+  console.log(skills[skills.length - 3]);
+  console.log(skills[skills.length - 4]);
+  console.log(skills[skills.length - 5]);
+  console.log(skills[skills.length - 6]);
+}
